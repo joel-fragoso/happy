2ca2ba5aa6cf2d9fda59c58070be2f5cae2f3d06
Joel Fragoso <joelfragoso85@icloud.com>
2020-10-12 13:06:35 -0300

    refactor: landing page

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/images/logo.svg';
 
-import { Container } from './styles';
+import { Container, Wrapper, Content, Location } from './styles';
 
 const Landing: React.FC = () => {
   return (
     <Container>
-      <img src={logoImg} alt="Happy Logo" />
+      <Wrapper>
+        <img src={logoImg} alt="Happy Logo" />
+        <Content>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </Content>
+        <Location>
+          <strong>Monte Sião</strong>
+          <span>Minas Gerais</span>
+        </Location>
+        <a href="/">
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+        </a>
+      </Wrapper>
     </Container>
   );
 };
--- web/src/pages/Landing/styles.ts
+++ web/src/pages/Landing/styles.ts
@@ -1,7 +1,82 @@
 import styled from 'styled-components';
 
+import landingImg from '../../assets/images/landing.svg';
+
 export const Container = styled.div`
   width: 100vw;
   height: 100vh;
   background: linear-gradient(329.54deg, #29b6d1 0%, #00c7c7 100%);
+`;
+
+export const Wrapper = styled.div`
+  position: relative;
+
+  width: 100%;
+  max-width: 1100px;
+
+  height: 100%;
+  max-height: 680px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+
+  background: url(${landingImg}) no-repeat right center;
+
+  > a {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+
+    width: 80px;
+    height: 80px;
+
+    background-color: #ffd666;
+    border-radius: 30px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    transition: background-color 0.2s;
+
+    &:hover {
+      background-color: #96feff;
+    }
+  }
+`;
+
+export const Content = styled.div`
+  max-width: 350px;
+
+  h1 {
+    font-size: 76px;
+    font-weight: 800;
+    line-height: 70px;
+  }
+
+  p {
+    margin-top: 40px;
+    font-size: 24px;
+    line-height: 34p;
+  }
+`;
+
+export const Location = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+
+  font-size: 24px;
+  line-height: 34px;
+
+  text-align: right;
+
+  display: flex;
+  flex-direction: column;
+
+  strong {
+    font-weight: 800;
+  }
 `;
