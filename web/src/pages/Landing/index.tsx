2ab1ca27a858d32db24d4784403af77b7fbffce6
Joel Fragoso <joelfragoso85@icloud.com>
2020-10-12 12:06:52 -0300

    chore: add assets folder

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import logoImg from '../../assets/images/logo.svg';
+
 import { Container } from './styles';
 
 const Landing: React.FC = () => {
   return (
     <Container>
-      <h1>Olá Mundo!</h1>
+      <img src={logoImg} alt="Happy Logo" />
     </Container>
   );
 };
