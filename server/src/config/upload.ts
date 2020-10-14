b32bab5a8b09439c786a3eb78a3aea9e7920dab6
Joel Fragoso <joelfragoso85@icloud.com>
2020-10-14 14:16:09 -0300

    chore: install multer

---
+++ server/src/config/upload.ts
@@ -0,0 +1,16 @@
+import multer from 'multer';
+import path from 'path';
+import crypto from 'crypto';
+
+export default {
+  storage: multer.diskStorage({
+    destination: path.join(__dirname, '..', '..', 'tmp'),
+    filename: (request, file, callback) => {
+      const fileHash = crypto.randomBytes(10).toString('hex');
+
+      const fileName = `${fileHash}-${file.originalname}`;
+
+      callback(null, fileName);
+    },
+  }),
+};
--- server/src/modules/orphanages/infra/http/routes/orphanages.routes.ts
+++ server/src/modules/orphanages/infra/http/routes/orphanages.routes.ts
@@ -1,14 +1,19 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from '../../../../../config/upload';
 
 import OrphanagesController from '../controllers/OrphanagesController';
 
 const orphanagesRouter = Router();
 
+const upload = multer(uploadConfig);
+
 const orphanagesController = new OrphanagesController();
 
 orphanagesRouter.get('/', orphanagesController.index);
 
-orphanagesRouter.post('/', orphanagesController.create);
+orphanagesRouter.post('/', upload.array('images'), orphanagesController.create);
 
 orphanagesRouter.get('/:id', orphanagesController.show);
 
